293076e1e8e3132377c6ae73fa2a649f5b603b28
abmurli <abmurli.career@gmail.com>
2017-12-07 21:52:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello!\nWelcome to the new world of helms-v1')
+  response.send('Hello')
 })
 
 app.listen(app.get('port'), function() {
