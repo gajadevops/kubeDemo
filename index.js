dd46e3d9383c58820936e18d8c93deaa213e2eb2
abmurli <abmurli.career@gmail.com>
2017-12-07 21:19:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello!\nWelcome to the new world of helms-v1')
-})
+  response.send('Hello!\nWelcome to the new world of helms-v0')
+}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
